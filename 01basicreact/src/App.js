265c7c7d77382bceeb118027ade591e0d0b1455b
iamnikhilrajput@gmail.com <iamnikhilrajput@gmail.com>
2023-09-24 03:20:31 +0530

    "First Project"

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <h1>Nikhil Rajput | Create-React-App | Learned how to create react app using "npx create-react-app" and "npm create vite@latest". Both are similar with the basic diffence of 'node _modules' folder created along with 'create-react-app' and with 'vite' it has to be installed inside the directory manually. Then started the app in both using "npm run start" and "npm run dev" scripts written in "package.json" file for 'create-react-app' and 'vite' respectively. That's all for now. Thank you.</h1>
+  );
+}
+
+
+
+export default App;
